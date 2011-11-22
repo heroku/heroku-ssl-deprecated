11f96a841b15ffc3796d0d9a1ad4ee3affb720ab
Pedro Belo <pedro@heroku.com>
2011-11-22 15:08:37 -0800

    updating urls, new api moved to v3

--- lib/ssl/heroku/client.rb
+++ lib/ssl/heroku/client.rb
@@ -4,14 +4,14 @@ class Heroku::Client
   alias_method :clear_ssl_old,  :clear_ssl
 
   def ssl_list(app)
-    json_decode(get("v2/apps/#{app}/ssl", :accept => :json).to_s)
+    json_decode(get("v3/apps/#{app}/ssl", :accept => :json).to_s)
   end
 
   def ssl_add(app, pem, key)
-    json_decode(post("v2/apps/#{app}/ssl", :accept => :json, :pem => pem, :key => key).to_s)
+    json_decode(post("v3/apps/#{app}/ssl", :accept => :json, :pem => pem, :key => key).to_s)
   end
 
   def ssl_remove(app, cname)
-    json_decode(delete("v2/apps/#{app}/ssl/#{escape(cname)}", :accept => :json).to_s)
+    json_decode(delete("v3/apps/#{app}/ssl/#{escape(cname)}", :accept => :json).to_s)
   end
 end
